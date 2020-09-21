03661367113afb441d143ca2a227e19de8a92787
wafihfzhn <hafizhuddin96@gmail.com>
2020-09-21 08:45:49 +0700

    Tidy up

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,23 +9,10 @@ require("@rails/activestorage").start()
 require("channels")
 
 window.addEventListener('load', () => {
-  navigator.serviceWorker.register('/service-worker.js').then(registration => {
-    console.log('ServiceWorker registered: ', registration);
-
-    var serviceWorker;
-    if (registration.installing) {
-      serviceWorker = registration.installing;
-      console.log('Service worker installing.');
-    } else if (registration.waiting) {
-      serviceWorker = registration.waiting;
-      console.log('Service worker installed & waiting.');
-    } else if (registration.active) {
-      serviceWorker = registration.active;
-      console.log('Service worker active.');
-    }
-  }).catch(registrationError => {
-    console.log('Service worker registration failed: ', registrationError);
-  });
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(registration => {
+      console.log('ServiceWorker registered: ', registration)
+    });
 });
 
 // Uncomment to copy all static images under ../images to the output folder and reference
