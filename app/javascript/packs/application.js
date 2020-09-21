9ca0f4f1a431ea2548090cf58a19853c872b85f4
wafihfzhn <hafizhuddin96@gmail.com>
2020-09-21 07:30:58 +0700

    Modify home

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,12 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+window.addEventListener('load', () => {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(registration => {
+      console.log('ServiceWorker registered: ', registration)
+    });
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
